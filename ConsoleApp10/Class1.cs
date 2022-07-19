fc24e5c4fbbaf1c753aa5bdd38a23ce5cfa71dfd
User <User@WIN-0I743UKFDV2>
2022-07-19 20:13:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLife
+{
+    internal class Life
+    {
+        public readonly int rows;
+        public readonly int columns;
+        private bool[,] arr;
+        public Life(int rows, int columns)      //первоначальное построение
+        {
+            this.rows = rows;
+            this.columns = columns;
+            arr = new bool[columns, rows];
+            Random rand = new Random();
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    arr[x, y] = rand.Next(5) == 0;
+                }
+            }
+        }
+        public bool[,] LifeSpot()       //выявление всех клеток которые живы
+        {
+            var result = new bool[columns, rows];
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    result[x, y] = arr[x, y];
+                }
+            }
+            return result;
+        }
+        public void NextLife()              //генерация новых клеток исходя из состояния старых
+        {
+            var newArr = new bool[columns, rows];
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    var sosed = CheckSosed(x, y);
+                    var alive = arr[x, y];
+                    if (!alive && sosed == 3)
+                    {
+                        newArr[x, y] = true;
+                    }
+                    else if (alive && (sosed < 2 || sosed > 3))
+                    {
+                        newArr[x, y] = false;
+                    }
+                    else
+                    {
+                        newArr[x, y] = arr[x, y];
+                    }
+                }
+            }
+            arr = newArr;
+
+        }
+        private int CheckSosed(int x, int y)            //проверка соседей клеток и по бокам и по диагонали
+        {
+            int count = 0;
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    var col = (x + i + columns) % columns;
+                    var row = (y + j + rows) % rows;
+                    var curentspot = col == x & row == y;
+                    var alive = arr[col, row];
+                    if (alive && !curentspot)
+                    {
+                        count++;
+                    }
+
+                }
+            }
+            return count;
+        }
+    }
+}
+
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLife
+{
+    public static class Programm
+    {
+        public static void Main()
+        {
+            Console.ReadLine();
+            Console.SetCursorPosition(0, 0);
+            var life = new Life(83, 315);
+            Console.ForegroundColor = ConsoleColor.Red;
+            while (true)
+            {
+                var arr = life.LifeSpot();
+                Console.SetCursorPosition(0, 0);
+                for (int y = 0; y < arr.GetLength(1); y++)
+                {
+                    var cha = new char[arr.GetLength(0)];
+                    for (int x = 0; x < arr.GetLength(0); x++)
+                    {
+                        if (arr[x, y])
+                            cha[x] = '*';
+                        else
+                            cha[x] = ' ';
+                    }
+                    Console.WriteLine(cha);
+                }
+
+                life.NextLife();
+            }
+        }
+    }
+}
